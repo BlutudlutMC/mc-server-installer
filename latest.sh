1ed1fef7d18829aeecc30590d8d08900906b2a10
BlutudlutMC <86069069+BlutudlutMC@users.noreply.github.com>
2022-04-19 15:15:35 +0200

    Update latest.sh

--- latest.sh
+++ latest.sh
@@ -1,64 +1,154 @@
 #!/bin/bash
-ram='1024'
+SERVER_RAM='1536M'
 COLOR_GREEN='\033[0;32m'
-apt update && apt upgrade -y
-clear
-sleep 1
-echo "This is a Minecraft Server install script."
-sleep 3
-clear
-echo "${Green}Installing required Modules..."
-apt -qq install wget curl sudo -y 
-apt -qq install zip tar gzip unzip -y
-echo "${Green}Modules installed!"
-sleep 1
-echo "${Green}Creating directories and setting up."
-mkdir -p /srv/temp/mcserver/
-cd /srv/temp/mcserver/
-echo "${Green}Directory created!"
-echo "${Green}You selected Version 1.17.1!"
-sleep 1
-echo "${Green}Installing Java 17"
-sleep 1
-echo "${Green}Using SDKMan... Installing..."
-curl -s "https://get.sdkman.io" | bash
-echo "${Green}Script downloaded..."
-echo "${Green}Setting Home Variable..."
-sleep 1
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-echo "${Green}Home Variable set..."
-sleep 1
-sdk version
-echo "${Green}SDKMan Installed..."
-apt install openjdk-17-jre-headless -y
-echo "${Green}Downloading Server Files..."
-cd /srv/temp/mcserver/
-curl -Lo server.jar https://cloud.blutudlut.xyz/index.php/s/3qJ6ZG5kePWBZyk/download
-mv server.jar server1.17.1.jar
-cd
-mkdir -p MCServer
-while true; do
-    read -p "Do you agree the Minecraft® EULA? (Required) (Y/N) " yn
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_WHITE='\033[0m'
+EULA=''
+DIRECTORY='mcserver'
+echo -e "${COLOR_WHITE}############################"
+echo -e "${COLOR_WHITE}#         MCServer         #"
+echo -e "${COLOR_WHITE}#      Install Script      #"
+echo -e "${COLOR_WHITE}#                          #"
+echo -e "${COLOR_WHITE}#       by Blutudlut       #"
+echo -e "${COLOR_WHITE}#            V2            #"
+echo -e "${COLOR_WHITE}#                          #"
+echo -e "${COLOR_WHITE}#       MC: 1.17.1         #"
+echo -e "${COLOR_WHITE}############################"
+echo "* Continue?"
+select yn in "Yes" "No"; do
     case $yn in
-        [Yy]* ) echo "Minecraft® EULA successfully agreed."; break;;
-        [Nn]* ) echo "To run the server you must agree the Minecraft® EULA. Exiting..."; exit;;
-        * ) echo "Please answer yes!";;
+        Yes ) echo -e "${COLOR_WHITE}OK..."; break;;
+        No ) echo -e "${COLOR_WHITE}OK... "; echo "Exiting..."; exit;;
     esac
 done
-cd MCServer
-echo "eula=true" > eula.txt
-cp /srv/temp/mcserver/server1.17.1.jar server.jar
-echo "${Green}How much RAM do you want for your MCServer? Default: 1024 (Do not enter M or MB!)"
-read ram
-echo "$ram is now your MCServer RAM."
-echo "java -Xmx$ram M -jar server.jar nogui"
-echo "java -Xmx${ram}M -jar server.jar nogui" > start.sh
-echo "Your Minecraft Server is now installed successfully!"
-echo "You can start your server by typing in: cd $HOME/MCServer/ && bash start.sh"
-echo "THANKS FOR USING THIS SCRIPT! <3"
-echo "Changelog:"
-echo "1.01 Hotfix"
-echo "Fixed start.sh Script"
-echo "Updated Java Installation"
-echo "Updated Install Information"
-echo "For more infos visit our Github"
+clear
+apt update -y
+clear
+apt install neofetch -y
+sleep 1
+clear
+sleep 1
+clear
+neofetch
+sleep 0.3
+echo "* Do you met these Requirements?:"
+sleep 1
+sleep 0.1
+echo -e "${COLOR_WHITE}############################"
+sleep 0.1
+echo -e "${COLOR_WHITE}#         1 GB RAM         #"
+sleep 0.1
+echo -e "${COLOR_WHITE}#        1 CPU CORE        #"
+sleep 0.1
+echo -e "${COLOR_WHITE}#         5 GB Disk        #"
+sleep 0.1
+echo -e "${COLOR_WHITE}############################"
+sleep 0.1
+echo "* Do you met these Requirements?:"
+sleep 1
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) clear && sleep 1 && echo -e "${COLOR_WHITE}Installing... " && sleep 0.2; break;;
+        No ) echo -e "${COLOR_WHITE}OK... "; echo "Exiting..."; exit;;
+    esac
+done
+clear
+sleep 1
+echo "* Creating directories..."
+echo "* DEBUG: mkdir -p /srv/temp/mcserver/serverfiles/"
+mkdir -p /srv/temp/mcserver/serverfiles/
+echo "* DEBUG: cd /srv/temp/mcserver/serverfiles/"
+cd /srv/temp/mcserver/serverfiles/
+echo "* Directories created..."
+clear
+echo "* Downloading Serverfiles..."
+echo "* You selected Minecraft Server Version 1.17.1"
+sleep 1
+echo "* Fetching server.jar from Cloud..."
+echo "* DEBUG: Fetch: cloud.blutudlut.xyz"
+curl --silent -Lo server.jar https://cloud.blutudlut.xyz/index.php/s/3KZkykcP8HrH2rC/download
+echo "* Fetching done..."
+sleep 2
+clear
+echo "* Fetching server.properties from Cloud..."
+echo "* DEBUG: Fetch: cloud.blutudlut.xyz"
+curl --silent -Lo server.properties https://cloud.blutudlut.xyz/index.php/s/xHdAwj4xJBpbJrW/download
+echo "* Fetching done..."
+sleep 2
+clear
+echo "* Fetching eula.txt from Cloud..."
+echo "* DEBUG: Fetch: cloud.blutudlut.xyz"
+curl --silent -Lo eula.txt https://cloud.blutudlut.xyz/index.php/s/TtmDkSyXQgRjZAp/download
+echo "* Fetching done..."
+sleep 2
+clear
+clear
+sleep 2
+clear
+echo "* Fetching start.sh from Cloud..."
+echo "* DEBUG: Fetch: cloud.blutudlut.xyz"
+curl --silent -Lo start.sh https://cloud.blutudlut.xyz/index.php/s/4N5mxESqAd8o6tg/download
+echo "* Fetching done..."
+sleep 2
+clear
+echo "* Updating System..."
+sleep 2
+apt update -y && apt upgrade -y
+clear
+echo "* Installing required Modules. (1/3)"
+sleep 2
+clear
+echo "* 1. JAVA"
+echo "* 1.1 Using Java 17"
+sleep 1
+apt update -y
+apt install openjdk-17-jre-headless -y
+clear
+echo "* Java installed!"
+sleep 2
+clear
+echo "* Installing required Modules. (2/3)"
+sleep 2
+echo "* 2. NANO & CURL"
+sleep 1
+apt update -y
+apt install nano curl -y
+clear
+echo "* Nano & cURL installed!"
+sleep 2
+clear
+echo "* Installing required Modules. (3/3)"
+sleep 2
+echo "* 3. WGET, SUDO, SCREEN"
+sleep 1
+apt update -y
+apt install wget sudo screen -y
+clear
+echo "* WGet, Sudo, Screen installed!"
+sleep 2
+clear
+echo "* Where is the directory for your MCServer Install Location? (Default mcserver/)"
+echo "* Please do not enter home/youruser"
+read DIRECTORY
+sleep 1
+echo "* ${HOME}/$DIRECTORY is now is now your install Location."
+sleep 3
+cd $HOME
+mkdir $DIRECTORY
+cp /srv/temp/mcserver/serverfiles/* $DIRECTORY
+clear
+sleep 1
+echo "* Copied!"
+echo "* Deleting TEMP Files."
+sleep 1
+rm -r /srv/temp/mcserver/serverfiles/
+rm -r /srv/temp/mcserver/
+clear
+echo "* Deleted!"
+sleep 1
+clear
+chmod 777 ${HOME}/${DIRECTORY}/start.sh
+echo "Your Minecraft Server is now succesfully installed! To start the server type in ${HOME}/${DIRECTORY}/start.sh"
+echo -e "${COLOR_GREEN}Thanks for using my Script! <3"
+echo -e "${COLOR_WHITE}"
